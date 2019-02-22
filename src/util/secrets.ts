86d5ebb46106daeee57975a27194f141aa4fa2e1
phico <philippe@collignon.email>
2019-02-22 11:29:31 +0100

    JWT_SECRET test

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -8,7 +8,7 @@ if (!MONGODB_URI) {
 
 export const JWT_SECRET = process.env["JWT_SECRET"];
 
-if (!MONGODB_URI) {
+if (!JWT_SECRET) {
     console.log("No JWT secret string. Set JWT_SECRET environment variable.");
     process.exit(1);
 }
